3846f025a912c21f7de79b027bb14e1cc853fad5
hinajamshaid <hinajamshediqbal@gmail.com>
2025-09-04 06:35:00 +0500

    input added

---
+++ core.py
@@ -0,0 +1,6 @@
+birth_year = int(input("Enter your birth year:"))
+birth_month = input("Enter your birth month:")
+# birth_date = int(input("Enter your birth date"))
+caculation = 2025 - birth_year
+
+print(f"You are {caculation} years {birth_month} months old  ")
