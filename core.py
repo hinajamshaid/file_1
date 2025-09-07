a335077ea11a53fc95b9ead4644f9daa11b4a991
hinajamshaid <hinajamshediqbal@gmail.com>
2025-09-07 07:10:22 +0500

    weight  to pounds

--- core.py
+++ core.py
@@ -1,6 +1,21 @@
-birth_year = int(input("Enter your birth year:"))
-birth_month = input("Enter your birth month:")
-# birth_date = int(input("Enter your birth date"))
-caculation = 2025 - birth_year
+# birth_year = int(input("Enter your birth year:"))
+# # birth_month = input("Enter your birth month:")
+# # birth_date = int(input("Enter your birth date"))
+# caculation = 2025 - birth_year
 
-print(f"You are {caculation} years {birth_month} months old  ")
+# print(f"You are {caculation} years old ")
+
+
+
+
+weight_lbs=int(input("Enter weight:"))
+weight_kg=weight_lbs*0.45
+print(f"You weigh {weight_kg} kilograms")
+offer_letter='''Hey Hina ,
+conglatulations you got selected in giki
+here this is our last email to you 
+please submit the fee 
+Thanks.'''
+print(offer_letter)
+
+
